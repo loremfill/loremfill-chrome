afebfdf34453e177b22e67820e66a75b524e9ef6
Sachin Sudheendra <sachin.sudheendra@gmail.com>
2015-06-07 01:02:32 +0530

    Not relying on a form attribute being present. Also only populating visible elements

--- filler.js
+++ filler.js
@@ -1,14 +1,13 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, callback) {
     if (msg.text && msg.text === 'fill_all_forms') {
         S.extendPrototype();
-        var forms = jQuery("form");
-        forms.each(function(i, form) {
-            var inputs = jQuery(form).find("input[type='text'], input[type='textarea'], input[type='password'], input[type='email'], input[type='number']");
-            inputs.each(function(j, input) {
-                input = jQuery(input)
+        var inputs = jQuery(document).find("input[type='text'], input[type='textarea'], input[type='password'], input[type='email'], input[type='number']");
+        inputs.each(function(j, input) {
+            input = jQuery(input)
+            if (input.is(":visible")) {
                 var value = decide(input);
                 input.val(value);
-            });
+            }
         });
     }
 });
@@ -20,7 +19,7 @@ var NameAttributeStrategy = {
         }
         var nameAttribute = input.attr('name');
         if (nameAttribute) {
-            if (nameAttribute.contains("name")) {
+            if (nameAttribute.contains("name") || nameAttribute.contains("Name")) {
                 return chance.name();
             }
             if (nameAttribute.contains("email")) {
