0389eb4798734a29cc5e876b968cd5ee181a7e80
Sachin Sudheendra <sachin.sudheendra@gmail.com>
2015-06-14 23:27:08 +0530

    Supporting keyboard shortcut to fill in page (Cmd+Shift+1)

--- background.js
+++ background.js
@@ -1,12 +1,20 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+function send_fill_all_forms(tab) {
     chrome.tabs.sendMessage(tab.id, {
         text: "fill_all_forms"
     }, function() {});
-});
+};
+
+function send_fill_single_element(tab) {
+    chrome.tabs.sendMessage(tab.id, {
+        text: "fill_single_element"
+    }, function() {});
+};
+
+chrome.browserAction.onClicked.addListener(send_fill_all_forms);
 
 chrome.contextMenus.create({
     id: "context_menu_lorem_fill_this_page",
-    title: "Lorem Fill this page",
+    title: "Lorem Fill this page ⌘⇧1",
     contexts: ["page"]
 });
 
@@ -18,13 +26,21 @@ chrome.contextMenus.create({
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
     if (info.menuItemId === "context_menu_lorem_fill_this_page") {
-        chrome.tabs.sendMessage(tab.id, {
-            text: "fill_all_forms"
-        }, function() {});
+        send_fill_all_forms(tab);
     }
     if (info.menuItemId === "context_menu_lorem_fill_this_element") {
-        chrome.tabs.sendMessage(tab.id, {
-            text: "fill_single_element"
-        }, function() {});
+        send_fill_single_element(tab);
+    }
+});
+
+chrome.commands.onCommand.addListener(function(command) {
+    var self=this;
+    chrome.tabs.query({
+        active: true
+    }, function(tabs) {
+        self.activeTab = tabs[0];
+    });
+    if (command === 'trigger_fill_all_forms' && self.activeTab) {
+        send_fill_all_forms(self.activeTab);
     }
 });
--- js/filler.js
+++ js/filler.js
@@ -39,4 +39,3 @@ function decide(input) {
     }
     return chance.sentence();
 }
-
