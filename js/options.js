11688b485123be6eb587c698745c417529566eab
Sachin Sudheendra <sachin.sudheendra@gmail.com>
2015-06-23 16:51:17 +0530

    Having a random id for the options page so that options.js does not interact with the active tab

--- js/options.js
+++ js/options.js
@@ -1,16 +1,16 @@
 jQuery(document).ready(function() {
     fill_options();
-    jQuery("[data-content]").popup();
-    jQuery(".submit.button").click(save_options);
-    jQuery(".reset.button").click(function() {
-        jQuery("input[name='password']").val(Defaults.password);
-        jQuery("input[name='domains']").val(Defaults.domains);
+    jQuery("#EnDLrDcjbUyKCvVThsPMuGfskEDFcK2fvBAJTDEUBvmee2AKC [data-content]").popup();
+    jQuery("#EnDLrDcjbUyKCvVThsPMuGfskEDFcK2fvBAJTDEUBvmee2AKC .submit.button").click(save_options);
+    jQuery("#EnDLrDcjbUyKCvVThsPMuGfskEDFcK2fvBAJTDEUBvmee2AKC .reset.button").click(function() {
+        get_option_element('password').val(Defaults.password);
+        get_option_element('domains').val(Defaults.domains);
     });
 });
 
 function save_options() {
-    var password = jQuery("input[name='password']").val();
-    var domains = jQuery("input[name='domains']").val();
+    var password = get_option_element('password').val();
+    var domains = get_option_element('domains').val();
     var payload = {
         password: password,
         domains: domains
@@ -20,7 +20,11 @@ function save_options() {
 
 function fill_options() {
     Store.refresh(function() {
-        jQuery("input[name='password']").val(Store.password);
-        jQuery("input[name='domains']").val(Store.domains);
+        get_option_element('password').val(Store.password);
+        get_option_element('domains').val(Store.domains);
     });
+}
+
+function get_option_element(name) {
+    return jQuery("#EnDLrDcjbUyKCvVThsPMuGfskEDFcK2fvBAJTDEUBvmee2AKC input[name='"+ name + "']");
 }
