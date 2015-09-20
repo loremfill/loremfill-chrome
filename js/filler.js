fb03b35c2454797c60a741b8b4e7077626bc8b72
Sachin Sudheendra <sachin.sudheendra@gmail.com>
2015-09-20 19:32:29 +0530

    Moving contents of listeners.js into filler.js and removing file

--- js/filler.js
+++ js/filler.js
@@ -1,14 +1,20 @@
+var contextElement = null;
+document.addEventListener("contextmenu", function(event) {
+    if (event && event.target) {
+        contextElement = event.target;
+    }
+});
+
 chrome.runtime.onMessage.addListener(function(msg, sender, callback) {
     Store.refresh();
     S.extendPrototype();
+    var elements = new Elements();
     if (msg.text && msg.text === 'fill_all_forms') {
-        var elements = new Elements();
         elements.perform();
     }
     if (msg.text && msg.text === "fill_single_element") {
         if (contextElement) {
             contextElement = $(contextElement);
-            var elements = new Elements();
             elements.process(contextElement);
         }
         if (callback) {
--- js/listeners.js
+++
@@ -1,6 +0,0 @@
-var contextElement = null;
-document.addEventListener("contextmenu", function(event) {
-    if (event && event.target) {
-        contextElement = event.target;
-    }
-});
